8445b98974ebf00d391c790526952a3531114ea1
Vandana Nandan Mishra <vnandan2001@gmail.com>
2021-12-17 14:30:04 +0530

    Landing Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import LandingPage from "./screens/LandingPages/LandingPage";
 
 const App = () => (
   <>
     {/* <h1 classname="App">Hello</h1> */}
     <Header />
+    <main>
+      <LandingPage />
+    </main>
     <Footer />
   </>
 );
---
+++ client/src/screens/LandingPages/LandingPage.js
@@ -0,0 +1,40 @@
+// import React, { useEffect } from "react";
+import { Button, Container, Row } from "react-bootstrap";
+// import { useSelector } from "react-redux";
+// import { Link } from "react-router-dom";
+import "./LandingPage.css";
+
+const LandingPage = () => {
+  return (
+    <div className="main">
+      <Container>
+        <Row>
+          <div className="intro-text">
+            <div>
+              <h1 className="title">Welcome to URL Shortner</h1>
+              <p className="subtitle">One place for all your URLs.</p>
+            </div>
+            <div className="buttonContainer">
+              <a href="#login">
+                <Button size="lg" className="landingbutton">
+                  Login
+                </Button>
+              </a>
+              <a href="#Signup">
+                <Button
+                  variant="outline-primary"
+                  size="lg"
+                  className="landingbutton"
+                >
+                  Signup
+                </Button>
+              </a>
+            </div>
+          </div>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default LandingPage;
