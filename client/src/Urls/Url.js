c1d4f6abd642d88288734c9dc0adab66841515fd
Vandana Nandan Mishra <vnandan2001@gmail.com>
2021-12-18 18:44:31 +0530

    rendering urls

---
+++ client/src/Urls/Url.js
@@ -0,0 +1,10 @@
+const urls = [
+  {
+    _id: "1",
+    Short_Url: "https://bit.ly/31ZNJFK",
+    Long_Url:
+      "https://www.google.com/search?q=Build+a+tool+where+user+can+generate+short+URL+for+their+long+URL+so+that+user+can+send+this+URL+in+SMS,+Email+and+track+user+activity.+Signup+Login+via+auth0+You+need+to+take+longUrl+as+an+input+param+and+returns+a+shortURL+via+UI+and+API+both.+Since+this+is+to+send+on+SMS,+Email,+every+URL+must+be+unique+to+identify+user+and+then+record+its+activity.+UI+of+reporting+%E2%86%92+how+many+clicked+and+on+what+browser+user+wise&rlz=1C1UEAD_enIN979IN979&oq=Build+a+tool+where+user+can+generate+short+URL+for+their+long+URL+so+that+user+can+send+this+URL+in+SMS,+Email+and+track+user+activity.+Signup+Login+via+auth0+You+need+to+take+longUrl+as+an+input+param+and+returns+a+shortURL+via+UI+and+API+both.+Since+this+is+to+send+on+SMS,+Email,+every+URL+must+be+unique+to+identify+user+and+then+record+its+activity.+UI+of+reporting+%E2%86%92+how+many+clicked+and+on+what+browser+user+wise&aqs=chrome..69i57.860j0j7&sourceid=chrome&ie=UTF-8",
+  },
+];
+
+module.exports = urls;
--- client/src/screens/MyUrls/MyUrls.js
+++ client/src/screens/MyUrls/MyUrls.js
@@ -1,8 +1,45 @@
 import React from "react";
+import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import MainScreen from "../../components/MainScreen";
+import urls from "../../Urls/Url";
 
 const MyUrls = () => {
-  return <MainScreen title="Welcome Back">Urls</MainScreen>;
+  return (
+    <MainScreen title="Welcome Back">
+      <Link to="createUrl">
+        <Button style={{ marginleft: 10, marginbottom: 6 }} size="lg">
+          {" "}
+          Create Url
+        </Button>
+        {urls.map((url) => (
+          <Card style={{ margin: 10 }}>
+            <Card.Header style={{ display: "flex" }}>
+              <span
+                style={{
+                  color: "black",
+                  textDecoration: "none",
+                  flex: 1,
+                  cursor: "pointer",
+                  alignSelf: "center",
+                  fontSize: 18,
+                }}
+              >
+                {url.Short_Url}
+              </span>
+
+              <div>
+                <Button>Edit</Button>
+                <Button varient="Danger" className="mx-2">
+                  Delete
+                </Button>
+              </div>
+            </Card.Header>
+          </Card>
+        ))}
+      </Link>
+    </MainScreen>
+  );
 };
 
 export default MyUrls;
