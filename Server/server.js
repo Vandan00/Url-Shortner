6a3db6efe1d84cf929c084f94c19085f1fbb4b19
Vandana Nandan Mishra <vnandan2001@gmail.com>
2021-12-19 01:05:50 +0530

    connecting db

--- Server/server.js
+++ Server/server.js
@@ -3,8 +3,11 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const urls = require("./Urls/Url");
+const mongoose = require("mongoose");
+// const connectDB = require("./config/db");
 
 dotenv.config();
+// connectDB();
 
 app.get("/", (req, res) => {
   res.send("API is running");
@@ -16,6 +19,20 @@ app.get("/api/url/:id", (req, res) => {
   const url = urls.find((n) => n._id === req.params.id);
   res.send(url);
 });
+
+// mongoose.set("useCreateIndex", true);
+mongoose
+  .connect(process.env.MongoDB_URL, {
+    dbName: process.env.MONGO_DB_Name,
+    user: process.env.DB_User,
+    pass: process.env.DB_Password,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Connection estabislished with MongoDB");
+  })
+  .catch((error) => console.error(error.message));
 
 const PORT = process.env.PORT || 5000;
 app.listen(5000, console.log(`Server started at port ${PORT}`));
