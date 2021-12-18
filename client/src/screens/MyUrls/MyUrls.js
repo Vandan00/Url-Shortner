9627766a7b3cc92dfe9aed3f71259ee09ec04775
Vandana Nandan Mishra <vnandan2001@gmail.com>
2021-12-18 19:29:17 +0530

    modifying url page

--- client/src/screens/MyUrls/MyUrls.js
+++ client/src/screens/MyUrls/MyUrls.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { Button, Card } from "react-bootstrap";
+import { Accordion, Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import MainScreen from "../../components/MainScreen";
 import urls from "../../Urls/Url";
 
 const MyUrls = () => {
+  const deleteHandler = (id) => {
+    if (window.confirm("Are you sure?")) {
+    }
+  };
+
   return (
     <MainScreen title="Welcome Back">
       <Link to="createUrl">
@@ -12,7 +17,9 @@ const MyUrls = () => {
           {" "}
           Create Url
         </Button>
-        {urls.map((url) => (
+      </Link>
+      {urls.map((url) => (
+        <Accordion>
           <Card style={{ margin: 10 }}>
             <Card.Header style={{ display: "flex" }}>
               <span
@@ -25,19 +32,34 @@ const MyUrls = () => {
                   fontSize: 18,
                 }}
               >
+                {/* <Accordion.Toggle as={Card.Text} variant="link" eventKey="0"> */}
                 {url.Short_Url}
+                {/* </Accordion.Toggle> */}
               </span>
 
               <div>
-                <Button>Edit</Button>
-                <Button varient="Danger" className="mx-2">
+                <Button href={`/url/${url._id}`}>Edit</Button>
+                <Button
+                  varient="Danger"
+                  className="mx-2"
+                  onClick={() => deleteHandler(url._id)}
+                >
                   Delete
                 </Button>
               </div>
             </Card.Header>
+            {/* <Accordion.Collapse eventKey="0"> */}
+            <Card.Body>
+              <blockquote className="blackquote mb-0">
+                <p>{url.Long_Url}</p>
+
+                <footer className="blackquote-footer">created on - date</footer>
+              </blockquote>
+            </Card.Body>
+            {/* </Accordion.Collapse> */}
           </Card>
-        ))}
-      </Link>
+        </Accordion>
+      ))}
     </MainScreen>
   );
 };
